38dede94206a55b5724b8a07cc8bb2b38105cde9
Oliver Schwarz <Oliver Schwarz@Cherokee>
2013-05-22 21:26:59 +0200

    Adding a simple configuration file template

---
+++ config-dist.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Configuration file template
+ * 
+ * A simple configuration file to contain the basic settings for using the
+ * Guild Wars 2 api client. How to use? Copy the file to ./config.php and
+ * fill in the required values.
+ * 
+ * @package Core
+ * @author  Oliver Schwarz <oliver.schwarz@gmail.com>
+ */
+
+/**
+ * Guild Wars 2 api endpoint
+ * 
+ * Protocol and URL of the Guild Wars 2 api without trailing slash. Example:
+ * https://api.guildwars2.com
+ * 
+ * @var string
+ */
+define('GW2API_ENDPOINT', '');
+
+/**
+ * Guild Wars 2 api version
+ * 
+ * Required for connecting to the correct api version. Currently there is
+ * only v1, may be useful later.
+ * 
+ * @var string
+ */
+define('GW2API_VERSION', '');
